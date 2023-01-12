1fb2dfe7f11867881cc82c90c2a911a19cc86a90
Nyrres <57801283+Nyrress@users.noreply.github.com>
2023-01-12 11:19:20 +0100

    g oublie la virgule ^^'

--- app/Http/Controllers/SocketsController.php
+++ app/Http/Controllers/SocketsController.php
@@ -15,7 +15,7 @@ class SocketsController {
             new Pusher(
                 config("broadcasting.connections.pusher.key"),
                 config("broadcasting.connections.pusher.secret"),
-                config("broadcasting.connections.pusher.app_id")
+                config("broadcasting.connections.pusher.app_id"),
                 []
             )
         );
