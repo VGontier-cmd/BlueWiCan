68a10c98dcfea16b23301dbdf042fa03fe88907f
Nyrres <57801283+Nyrress@users.noreply.github.com>
2023-01-13 12:43:41 +0100

    Update CustomWebSocketHandler.php

--- app/Handlers/CustomWebSocketHandler.php
+++ app/Handlers/CustomWebSocketHandler.php
@@ -7,33 +7,34 @@ use Ratchet\ConnectionInterface;
 use BeyondCode\LaravelWebSockets\Apps\App;
 use Ratchet\RFC6455\Messaging\MessageInterface;
 use Ratchet\WebSocket\MessageComponentInterface;
+use Illuminate\Support\Facades\Log;
 
 class CustomWebSocketHandler implements MessageComponentInterface
 {
 
     public function onOpen(ConnectionInterface $connection)
     {
-        error_log('WebSocketHandler - custom');
+        Log::alert('WebSocketHandler - custom');
         $socketId = sprintf('%d.%d', random_int(1, 1000000000), random_int(1, 1000000000));
         $connection->socketId = $socketId;
         $connection->app = App::findById('staging');
-        dd('ouiiiiiiiiiiiiiiiii');
+        Log::alert('WebSocketHandler - custom');
         echo sprintf("[server] %s connected.\n", spl_object_hash($connection));
     }
     
     public function onClose(ConnectionInterface $connection)
     {
-        echo sprintf("[server] %s disconnected.\n", spl_object_hash($connection));
+        Log::alert("[server] %s disconnected.\n" . spl_object_hash($connection));
     }
 
     public function onError(ConnectionInterface $connection, \Exception $e)
     {
-        echo sprintf("[error] %s\n", $e->getMessage());
+        Log::alert("[error] %s\n". $e->getMessage());
     }
 
     public function onMessage(ConnectionInterface $connection, MessageInterface $msg)
     {
-        error_log('WebSocketHandler - onMessage');
-       SendData::createEvent($msg);
+        Log::alert('WebSocketHandler - onMessage');
+        SendData::createEvent($msg);
     }
 }
