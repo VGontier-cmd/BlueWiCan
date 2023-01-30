ed240a97edf23d4e00909c28efa45461a0a0f199
Gimly-76 <theo.carel99@gmail.com>
2023-01-30 18:18:31 +0100

    add websockets config

---
+++ config/websockets.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'ws_host' => env('WS_HOST', 'localhost'),
+    'ws_port' => env('WS_PORT', '6001')
+
+];
--- resources/views/partials/_toast.blade.php
+++ resources/views/partials/_toast.blade.php
@@ -1,10 +1,13 @@
-<div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
+<div v-if="flashMessage" class="toast show" role="alert" aria-live="assertive" aria-atomic="true" data-level="${level}">
     <div class="toast-header">
-        <i class="bi bi-info-square-fill"></i>
-        <strong class="me-auto">Info</strong>
+        <i v-if="flashMessage.level === 'success'" class="bi bi-check-square-fill"></i>
+        <i v-if="flashMessage.level === 'info'" class="bi bi-info-square-fill"></i>
+        <i v-if="flashMessage.level === 'warning'" class="bi bi-exclamation-square-fill"></i>
+        <i v-if="flashMessage.level === 'error'" class="bi bi-exclamation-square-fill"></i>
+        <strong class="me-auto">@{{flashMessage.level}}</strong>
         <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
     <div class="toast-body">
-        <p>Exemple de toast</p>
+        <p>@{{flashMessage.message}}</p>
     </div>
 </div>
