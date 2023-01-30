0f3bc05768e2387818605d5c2a77da1a49171b2d
Gimly-76 <theo.carel99@gmail.com>
2023-01-30 19:09:35 +0100

    add toast in vue.js

--- resources/views/partials/_toast.blade.php
+++ resources/views/partials/_toast.blade.php
@@ -1,11 +1,11 @@
-<div v-if="flashMessage" class="toast show" role="alert" aria-live="assertive" aria-atomic="true" data-level="${level}">
+<div v-if="flashMessage" class="toast" role="alert" aria-live="assertive" aria-atomic="true" v-bind:data-level='flashMessage.level'>
     <div class="toast-header">
         <i v-if="flashMessage.level === 'success'" class="bi bi-check-square-fill"></i>
         <i v-if="flashMessage.level === 'info'" class="bi bi-info-square-fill"></i>
         <i v-if="flashMessage.level === 'warning'" class="bi bi-exclamation-square-fill"></i>
         <i v-if="flashMessage.level === 'error'" class="bi bi-exclamation-square-fill"></i>
         <strong class="me-auto">@{{flashMessage.level}}</strong>
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        <button v-on:click="hideToast" type="button" class="btn-close"></button>
     </div>
     <div class="toast-body">
         <p>@{{flashMessage.message}}</p>
