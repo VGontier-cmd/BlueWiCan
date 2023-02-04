71ca965565594984f05a652b83e5569ec01eb12b
Gimly-76 <theo.carel99@gmail.com>
2023-02-04 18:40:55 +0100

    add charts

--- app/Http/Controllers/CoreController.php
+++ app/Http/Controllers/CoreController.php
@@ -10,25 +10,85 @@ use App\DataTables\CanDatasDataTable;
 
 class CoreController extends Controller
 {
+    private $today;
+    private $month;
+
+    private $startOfDay;
+    private $endOfDay;
+
+    private $startOfMonth;
+    private $endOfMonth;
+
+    public function __construct() {
+        setlocale(LC_TIME, 'fr_FR.utf8');
+        $this->today = Carbon::parse(Carbon::today())->locale('fr_FR')->isoFormat('dddd Do MMMM');
+        $this->month = Carbon::parse(Carbon::today())->locale('fr_FR')->isoFormat('MMMM');
+
+        $this->startOfDay = Carbon::createFromTimestamp(strtotime('today'))->startOfDay();
+        $this->endOfDay = Carbon::createFromTimestamp(strtotime('tomorrow'))->startOfDay();
+
+        $this->startOfMonth = Carbon::createFromTimestamp(strtotime('today'))->startOfMonth();
+        $this->endOfMonth = Carbon::createFromTimestamp(strtotime('today'))->endOfMonth();
+    }
     /**
      * Permet d'afficher le dashboard
      */
-    public function dashboard() {
-        $startOfDay = Carbon::createFromTimestamp(strtotime('today'))->startOfDay();
-        $endOfDay = Carbon::createFromTimestamp(strtotime('tomorrow'))->startOfDay();
-        
+    public function dashboard() { 
         $page_title = 'Bienvenue,';
         $page_subtitle = 'Consultez les données envoyées directement depuis le BlueWiCan.';
         
-        $nb_trames = CanData::all()->count();
-        $nb_trames_today = CanData::whereBetween('created_at', [$startOfDay, $endOfDay])->count();
+        $nb_trames_total = CanData::all()->count();
+        $nb_trames_current_day = CanData::whereBetween('created_at', [$this->startOfDay, $this->endOfDay])->count();
+        $nb_trames_current_month = CanData::whereBetween('created_at', [$this->startOfMonth, $this->endOfMonth])->count();
+
+        $today = $this->today;
+        $month = $this->month;
+
+        $data_graph_day = $this->getTramesCurrentDay();
+        $data_graph_month = $this->getTramesCurrentMonth();
+
+        $labels_graph_month = [];
+        foreach ($data_graph_month as $data) {
+            $date = Carbon::createFromTimestamp(strtotime("today"))->startOfMonth()->addDay($data->day - 1);
+            $labels_graph_month[] = $date->locale('fr_FR')->isoFormat('dddd Do');
+        }
 
         return view('core.dashboard', compact(
             'page_title', 
             'page_subtitle', 
-            'nb_trames',
-            'nb_trames_today' 
+            'nb_trames_total',
+            'nb_trames_current_day',
+            'nb_trames_current_month',
+            'data_graph_day',
+            'data_graph_month',
+            'labels_graph_month',
+            'today',
+            'month'
         ));
+    }
+
+    /**
+     * Permet de récupérer les données enregistrées regroupées par l'heure de création pour
+     * le jour courant
+     */
+    private function getTramesCurrentDay() {
+        $data = CanData::whereBetween('created_at', [$this->startOfDay, $this->endOfDay])
+            ->groupBy(DB::raw('hour(created_at)'))
+            ->select(DB::raw('hour(created_at) as hour'), DB::raw('count(*) as count'))
+            ->get();
+        return $data;
+    }
+
+    /**
+     * Permet de récupérer les données enregistrées regroupées par le jour de création pour
+     * le mois courant
+     */
+    public function getTramesCurrentMonth() {
+        $data = CanData::whereBetween('created_at', [$this->startOfMonth, $this->endOfMonth])
+                    ->groupBy(DB::raw('day(created_at)'))
+                    ->select(DB::raw('day(created_at) as day'), DB::raw('count(*) as count'))
+                    ->get();
+        return $data;
     }
 
     /**
--- resources/views/core/dashboard.blade.php
+++ resources/views/core/dashboard.blade.php
@@ -1,56 +1,66 @@
 @extends('layouts.app')
 
+@push('head')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+@endpush
+
 @section('content')
-    <div class="row">
+    <div class="row mb-4">
         <div class="col-xl-7">
-            <div class="card">
+            <div class="card card-graph" :class="{ 'show': currentGraph == 'day' }">
+                <div class="card-header bg-white">
+                    <h5 class="card-title">Données enregistrées aujourd'hui</h5>
+                    <h6 class="card-subtitle text-muted">Nombre de trames enregistrées par heure ce {{ $today }}</h6>
+                </div>
+                <div class="card-body">
+                    <canvas id="today-chart"></canvas>
+                </div>
+            </div>
+
+            <div class="card card-graph" :class="{ 'show': currentGraph == 'month' }">
+                <div class="card-header bg-white">
+                    <h5 class="card-title">Données enregistrées dans le mois</h5>
+                    <h6 class="card-subtitle text-muted">Nombre de trames enregistrées par jour ce {{ $month }}</h6>
+                </div>
+                <div class="card-body">
+                    <canvas id="month-chart"></canvas>
+                </div>
             </div>
         </div>
         <div class="col-xl-5 ms-auto mt-xl-0">
-            <div class="row">
-                <div class="col-md-6">
+            <div class="row mb-4">
+                <div class="col-md-12">
                     <div class="card">
                         <div class="card-body text-center">
-                            <h1 class="text-gradient text-primary">
-                                <span>{{ $nb_trames }}</span>
+                            <h1 class="card-count"> 
+                                <span>{{ $nb_trames_total }}</span>
                             </h1>
                             <h6 class="mb-0 font-weight-bolder">Trames enregistrées</h6>
                             <p class="opacity-8 mb-0 text-sm">Total</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 mt-md-0 mt-4">
-                    <div class="card">
+            </div>
+            <div class="row">
+                <div class="col-md-6 mt-md-0">
+                    <div v-on:click="showCurrentDayGraph()" class="card card-clickable" :class="{ 'active': currentGraph == 'day' }">
                         <div class="card-body text-center">
-                            <h1 class="text-gradient text-primary"> 
-                                <span>{{ $nb_trames_today }}</span>
+                            <h1 class="card-count"> 
+                                <span>{{ $nb_trames_current_day }}</span>
                             </h1>
                             <h6 class="mb-0 font-weight-bolder">Trames enregistrées</h6>
                             <p class="opacity-8 mb-0 text-sm">Aujourd'hui</p>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row mt-4">
                 <div class="col-md-6">
-                    <div class="card">
+                    <div v-on:click="showCurrentMonthGraph()" class="card card-clickable" :class="{ 'active': currentGraph == 'month' }">
                         <div class="card-body text-center">
-                            <h1 class="text-gradient text-primary">
-                                <span>...</span>
+                            <h1 class="card-count"> 
+                                <span>{{ $nb_trames_current_month }}</span>
                             </h1>
-                            <h6 class="mb-0 font-weight-bolder">...</h6>
-                            <p class="opacity-8 mb-0 text-sm">...</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 mt-md-0 mt-4">
-                    <div class="card">
-                        <div class="card-body text-center">
-                            <h1 class="text-gradient text-primary">
-                                <span>...</span>
-                            </h1>
-                            <h6 class="mb-0 font-weight-bolder">...</h6>
-                            <p class="opacity-8 mb-0 text-sm">...</p>
+                            <h6 class="mb-0 font-weight-bolder">Trames enregistrées</h6>
+                            <p class="opacity-8 mb-0 text-sm">Ce mois-ci</p>
                         </div>
                     </div>
                 </div>
@@ -60,12 +70,99 @@
 @endsection
 
 @push('scripts')
-    <script>
-        new Vue({
-            el: "#app",
-            data: {},
-            mounted() {},
-            methods: {}
-        });
-    </script>
+<script>
+    new Vue({
+        el: "#app",
+        data: {
+            currentGraph: 'day'
+        },
+        mounted() {
+            this.showCurrentDayGraph()
+        },
+        methods: {
+            showCurrentMonthGraph() {
+                this.currentGraph = 'month'
+            },
+
+            showCurrentDayGraph() {
+                this.currentGraph = 'day'
+            }
+        }
+    });
+</script>
+
+<script>
+var ctx_today = document.getElementById('today-chart').getContext('2d');
+var todayChart = new Chart(ctx_today, {
+    type: 'bar',
+    data: {
+        labels: [
+            @foreach ($data_graph_day as $data)
+                '{{ $data->hour }}:00',
+            @endforeach
+        ],
+        datasets: [{
+            label: 'Nombre de trames enregistrées',
+            data: [
+                @foreach ($data_graph_day as $data)
+                    {{ round($data->count) }},
+                @endforeach
+            ],
+            backgroundColor: [
+                '#243783'
+            ],
+            borderColor: [
+                '#243783'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var ctx_month = document.getElementById('month-chart').getContext('2d');
+var monthChart = new Chart(ctx_month, {
+    type: 'bar',
+    data: {
+        labels: [
+            @foreach ($labels_graph_month as $label)
+                '{{ $label }}',
+            @endforeach
+        ],
+        datasets: [{
+            label: 'Nombre de trames enregistrées',
+            data: [
+                @foreach ($data_graph_month as $data)
+                    {{ round($data->count) }},
+                @endforeach
+            ],
+            backgroundColor: [
+                '#243783'
+            ],
+            borderColor: [
+                '#243783'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+</script>
 @endpush
